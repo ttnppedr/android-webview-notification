34a10a969adaf82b90d3fb2f9c09cf2f7c1f66e0
ttnppedr <priest0204@gmail.com>
2025-01-10 21:35:39 +0800

    3

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email'    => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (!Auth::attempt($credentials)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token = $request->user()->createToken('API Token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
+});
