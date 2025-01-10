445ad2749a50e55db147b7370a7249e4b032daca
ttnppedr <priest0204@gmail.com>
2025-01-10 21:44:01 +0800

    4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\UserDevices;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -22,3 +23,22 @@ Route::post('/login', function (Request $request) {
 
     return response()->json(['token' => $token]);
 });
+
+Route::post('/devices', function (Request $request) {
+    $data = $request->validate([
+        'device_id' => 'required|string',
+    ]);
+
+    UserDevices::create([
+        'user_id' => $request->user()->id,
+        'device_id' => $data['device_id'],
+    ]);
+
+    return response(null, 201);
+})->middleware('auth:sanctum');
+
+Route::post('/notify', function (Request $request) {
+    $request->user();
+
+    return response('success', 200);
+})->middleware('auth:sanctum');
