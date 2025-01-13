a976359aedfd56b799f4ea3a3ec237ac942afebe
ttnppedr <priest0204@gmail.com>
2025-01-13 14:52:46 +0800

    remove force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
